a39fe7d9548bee746c74fb5f029d8d2a5e35b513
Foxocube <git@foxocube.xyz>
2025-11-12 19:59:14 +0000

    Try to fix time zones saying things happened 2000 years ago

---
+++ BlazorInventory/BlazorInventory/Data/Migrations/20251111230232_timezone_aware.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventorySharp.Migrations
+{
+    /// <inheritdoc />
+    public partial class timezone_aware : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX__Events_DelayUntil",
+                table: "_Events");
+
+            migrationBuilder.CreateIndex(
+                name: "IX__Events_DelayUntil_State",
+                table: "_Events",
+                columns: new[] { "DelayUntil", "State" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX__Events_DelayUntil_State",
+                table: "_Events");
+
+            migrationBuilder.CreateIndex(
+                name: "IX__Events_DelayUntil",
+                table: "_Events",
+                column: "DelayUntil");
+        }
+    }
+}
